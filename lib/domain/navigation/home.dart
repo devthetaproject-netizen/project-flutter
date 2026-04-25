591294f8cdda690911fee918dbbc4f480bc2cb01
dhea224 <dheaandhika1@gmail.com>
2026-04-26 00:02:43 +0700

    update navigasi part1

--- lib/domain/navigation/home.dart
+++ lib/domain/navigation/home.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_user/presentation/screens/promo_page.dart';
 
 class HomeNavigation {
-  gopromopage(BuildContext context) {
+  void gopromopage(BuildContext context) {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => const PromoPage()),
